dc4150e3e0d972e05325ebf02d8da515e91ae83a
jaekyung <jaekyung43@naver.com>
2022-07-17 23:26:05 +0900

    0717

--- python_basic/01_basic/day01_01_test.py
+++ python_basic/01_basic/day01_01_test.py
@@ -1,1 +1,10 @@
-print('hello!')
+'''
+2*1 = 2
+2*2 = 4
+2*3 = 6
+'''
+for i in range(2,10):
+    print(i,'단 =======================')
+    for j in range(1,10):
+        
+        print(i,'*', j, '=', i*j)
